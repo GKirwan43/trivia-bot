42879f24d1ee30f3b1649ec7aebb82ab47533a09
GKirwan43 <38820290+GKirwan43@users.noreply.github.com>
2018-06-09 18:18:12 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("on " +  client.guilds.size + " servers");
 });
 
 var prefix="!";
@@ -10,10 +11,27 @@ var prefix="!";
 client.on('message', msg => {
   if (msg.content === prefix + 'ping') {
     msg.channel.send({embed: {
-        color: 3447003,
+        color: 0x00ff00,
         title: "Pong! 🏓",
         description: client.ping + "ms"
     }});
+  }
+  if (msg.content.split(" ", 3)[0] === prefix + 'ban') {
+    if (msg.content.split(" ", 3)[1] != null && msg.content.split(" ", 3)[2] != null) {
+        let member = msg.mentions.members.first();
+        msg.channel.send(member.toString());
+    }
+    else
+    {
+        msg.channel.send({embed: {
+            color: 0xff0000,
+            title: "Invalid Syntax",
+            description: "Please use this format: !ban <User> <Reason>"
+        }});
+    }  
+  }
+  if (msg.mentions.members.first() != null && msg.mentions.members.first() == "<@455066126691467285>") {
+    msg.channel.send(msg.member.toString());
   }
 });
 
