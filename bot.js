5b1cee85efe758a88955aa80f2845a9208539677
GKirwan43 <38820290+GKirwan43@users.noreply.github.com>
2018-06-10 12:29:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,32 +6,89 @@ client.on('ready', () => {
   client.user.setActivity("on " +  client.guilds.size + " servers");
 });
 
-var prefix="!";
+var prefix="+";
 
-client.on('message', msg => {
-  if (msg.content === prefix + 'ping') {
-    msg.channel.send({embed: {
+client.on('message', message => {
+  if (message.content === prefix + 'ping') {
+    message.channel.send({embed: {
         color: 0x00ff00,
         title: "Pong! 🏓",
         description: client.ping + "ms"
     }});
   }
-  if (msg.content.split(" ", 3)[0] === prefix + 'ban') {
-    if (msg.content.split(" ", 3)[1] != null && msg.content.split(" ", 3)[2] != null) {
-        let member = msg.mentions.members.first();
-        msg.channel.send(member.toString());
+  if (message.content.split(" ", 3)[0] === prefix + 'mute') {
+    if (message.content.split(" ", 3)[1] != null && message.content.split(" ", 3)[2] != null) {
+        let member = message.mentions.members.first();
+        member.setMute(true, message.content.split(" ", 3)[2]);
+        const embed = new Discord.RichEmbed()
+        .setColor(0x00ff00)
+        .setTitle("Moderation")
+        .setDescription(member.toString() + " has been muted for: " + message.content.split(" ", 3)[2])
+        message.channel.send({embed});
     }
     else
     {
-        msg.channel.send({embed: {
+        message.channel.send({embed: {
             color: 0xff0000,
             title: "Invalid Syntax",
-            description: "Please use this format: !ban <User> <Reason>"
+            description: "Please use this format: " + prefix + "kick <User> <Reason>"
         }});
     }  
   }
-  if (msg.mentions.members.first() != null && msg.mentions.members.first() == "<@455066126691467285>") {
-    msg.channel.send(msg.member.toString());
+  if (message.content.split(" ", 3)[0] === prefix + 'kick') {
+    if (message.content.split(" ", 3)[1] != null && message.content.split(" ", 3)[2] != null) {
+        let member = message.mentions.members.first();
+        member.kick(message.content.split(" ", 3)[2]);
+        const embed = new Discord.RichEmbed()
+        .setColor(0x00ff00)
+        .setTitle("Moderation")
+        .setDescription(member.toString() + " has been kicked for: " + message.content.split(" ", 3)[2])
+        message.channel.send({embed});
+    }
+    else
+    {
+        message.channel.send({embed: {
+            color: 0xff0000,
+            title: "Invalid Syntax",
+            description: "Please use this format: " + prefix + "kick <User> <Reason>"
+        }});
+    }  
+  }
+  if (message.content.split(" ", 3)[0] === prefix + 'ban') {
+    if (message.content.split(" ", 3)[1] != null && message.content.split(" ", 3)[2] != null) {
+        let member = message.mentions.members.first();
+        member.ban({reason: message.content.split(" ", 3)[2]});
+        const embed = new Discord.RichEmbed()
+        .setColor(0x00ff00)
+        .setTitle("Moderation")
+        .setDescription(member.toString() + " has been banned for: " + message.content.split(" ", 3)[2])
+        message.channel.send({embed});
+    }
+    else
+    {
+        message.channel.send({embed: {
+            color: 0xff0000,
+            title: "Invalid Syntax",
+            description: "Please use this format: " + prefix + "ban <User> <Reason>"
+        }});
+    }  
+  }
+  if (message.mentions.members.first() != null && message.mentions.members.first() == "<@455066126691467285>") {
+    message.channel.send(message.member.toString());
+  }
+  if (message.content == prefix + "help") {
+    const embed = new Discord.RichEmbed()
+    .setColor(0x00ff00)
+    .setAuthor("Trivia Bot Commands","https://www.triviahelpers.com/GK.png")
+    .setDescription("Command Prefix is: '" + prefix + "'\n" 
+    + "**__Main Commands__**\n"
+    + "**" + prefix + "Help:** Shows every command.\n" 
+    + "**" + prefix + "Ping:** Check your ping to Trivia Bot.\n" 
+    + "**__Moderation Commands__**\n"
+    + "**" + prefix + "Kick <User> <Reason>:** Kicks a user.\n"
+    + "**" + prefix + "Ban <User> <Reason>:** Bans a user.")
+    .setThumbnail("https://www.triviahelpers.com/GK.png")
+    message.channel.send({embed});
   }
 });
 
