6b96c7e0f776c8d95f3894b82c6732d8d34542d7
GKirwan43 <38820290+GKirwan43@users.noreply.github.com>
2018-06-23 12:55:25 -0500

    Delete bot.js

--- bot.js
+++
@@ -1,9 +0,0 @@
-exports.run = (Discord,client, message, content) => {
-    if (content === "ping") {
-        const embed = new Discord.RichEmbed()
-        .setColor(0x00ff00)
-        .setTitle("Pong! 🏓")
-        .setDescription(client.ping + "ms")
-        message.channel.send({embed})
-    }
-}
