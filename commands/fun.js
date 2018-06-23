9def5a560d3980b186051741b4a04ebd0504799d
GKirwan43 <38820290+GKirwan43@users.noreply.github.com>
2018-06-23 14:33:48 -0500

    Update fun.js

--- commands/fun.js
+++ commands/fun.js
@@ -1,28 +1,29 @@
 exports.run = (Discord,client,prefix,message, content) => {
     const fs = require("fs");
-    let userData = JSON.parse(fs.readFileSync("./userData.json", "utf8"));
+    const mysql = require("mysql");
+    var con = mysql.createConnection({
+        host: "66.147.240.181",
+        user: "dsheetco_Bot",
+        password: "Crabisland1",
+        database: "dsheetco_userData"
+    });
+    con.connect(err => {
+        if (err) throw err;
+        console.log("Connected");
+    });
     var sections = content.split(" ");
     command = sections[0];
     if (command === "fish") {
-        if (!userData[message.author.id]) {
-            userData[message.author.id] = {
-                commonFish: 0,
-                uncommonFish: 0,
-                rareFish: 0
-            };
-            fs.writeFile("./userData.json", JSON.stringify(userData), (err) => console.error);
-        }
         function randomFish() {
             var random = Math.floor((Math.random()*100) + 1);
-            if (random >= 30) {userData[message.author.id].commonFish++; return "Common";}
-            if (random > 1 && random < 30) {userData[message.author.id].uncommonFish++; return "Uncommon";}
-            if (random === 1) {userData[message.author.id].rareFish++; return "Rare";}
+            if (random >= 30) {return "Common";}
+            if (random > 1 && random < 30) {return "Uncommon";}
+            if (random === 1) {return "Rare";}
         }
         const embed = new Discord.RichEmbed()
         .setColor(0x00ff00)
         .setTitle("Fishy")
         .setDescription(message.author + ", you caught a " + randomFish() + "!")
         message.channel.send({embed})
-        fs.writeFile("./userData.json", JSON.stringify(userData), (err) => console.error);
     }
 }
